c9e3a3a8c2443fe3894634542042d91f6706e26e
Florcoronel <70342926+Florcoronel@users.noreply.github.com>
2023-10-24 23:54:03 -0300

    try de add

--- src/controller.js
+++ src/controller.js
@@ -24,8 +24,19 @@ class LibroController{
     
     async add(req, res){
         const libro = req.body;
-        const [result] = await pool.query(`INSERT INTO libros(nombre,autor,categoria,añopublicacion,ISBN) VALUES (?,?,?,?,?)`,[libro.nombre, libro.autor, libro.categoria, libro.añopublicacion, libro.isbn]);
-        res.json({"id insertado": result.insertId})
+        try {
+            if (libro.nombre && libro.autor && libro.categoria && libro.añopublicacion && libro.isbn){ 
+            const [result] = await pool.query(`INSERT INTO libros(nombre,autor,categoria,añopublicacion,ISBN) VALUES (?,?,?,?,?)`,[libro.nombre, libro.autor, libro.categoria, libro.añopublicacion, libro.isbn]);
+            res.json({"id insertado": result.insertId})
+         }
+         else { 
+      res.status(404).json ({"Mensaje": "Falta rellenar campos"})
+        }
+        }
+        catch (error) {
+            console.error("Error al buscar el libro por ID:", error);
+            res.status(500).json({ "Mensaje": "Error en el servidor" });
+        }
     }
     async delete(req, res){
         const libro = req.body;
